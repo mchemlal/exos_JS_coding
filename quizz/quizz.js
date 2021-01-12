6d8a77edbaab2cfaec748ed333be846563ccfd65
mchemlal <michel.chemlal@gmail.com>
2021-01-12 12:08:34 +0100

    modif array js  quizz

--- quizz/quizz.js
+++ quizz/quizz.js
@@ -1,88 +1,48 @@
 let questions = [{
-    quetsion1: [{
-            question: "qui le présedent de France ?"
+        question: "What is 10/2?",
+        answers: {
+            a: '3',
+            b: '5',
+            c: '115'
         },
-        { suggestion1: "Emmanuel Macron" },
-        { suggestion2: "Boutlefica" },
-        {
-            suggestion3: "Omar Bongo"
+        correctAnswer: 'b'
+    },
+    {
+        question: "What is 30/3?",
+        answers: {
+            a: '3',
+            b: '5',
+            c: '10'
+        },
+        correctAnswer: 'c'
 
-        },
-    ],
-    question2: [{
-            question: "qui le présedent de France ?"
-        },
-        { suggestion1: "Emmanuel Macron" },
-        { suggestion2: "Boutlefica" },
-        {
-            suggestion3: "Omar Bongo"
 
-        },
-    ],
-    question3: [{
-            question: "qui le présedent de France ?"
-        },
-        { suggestion1: "Emmanuel Macron" },
-        { suggestion2: "Boutlefica" },
-        {
-            suggestion3: "Omar Bongo"
 
-        },
-    ],
-    question4: [{
-            question: "qui le présedent de France ?"
-        },
-        { suggestion1: "Emmanuel Macron" },
-        { suggestion2: "Boutlefica" },
-        {
-            suggestion3: "Omar Bongo"
-        },
-    ],
-    question5: [{
-            question: "qui le présedent de France ?"
-        },
-        { suggestion1: "Emmanuel Macron" },
-        { suggestion2: "Boutlefica" },
-        {
-            suggestion3: "Omar Bongo"
-        },
-    ],
-    question6: [{
-            question: "qui le présedent de France ?"
-        },
-        { suggestion1: "Emmanuel Macron" },
-        { suggestion2: "Boutlefica" },
-        {
-            suggestion3: "Omar Bongo"
-
-        },
-    ],
-    question7: [{
-            question: "qui le présedent de France ?"
-        },
-        { suggestion1: "Emmanuel Macron" },
-        { suggestion2: "Boutlefica" },
-        {
-            suggestion3: "Omar Bongo"
-        },
-    ],
-}, ];
-
-questions = questions[0];
-
-let question7 = questions.question7;
-for (element of question7) {
-    let questionNumero7 = document.createElement("div");
-    //let suggestion1 = document.createElement("input");
-    questionNumero7.setAttribute("for", "question0-7");
-    // suggestion1.setAttribute()
-    questionNumero7.innerHTML = "<label>" + element.question + "</label>";
-    let getDiv7 = document.querySelector("body");
-    getDiv7.appendChild(questionNumero7);
-    for (value of element) {
-        let suggestion
     }
+];
+
+// questions = questions[0];
+
+// let question7 = questions.question7;
+// for (element of question7) {
+//     let questionNumero7 = document.createElement("div");
+//     //let suggestion1 = document.createElement("input");
+//     questionNumero7.setAttribute("for", "question0-7");
+//     // suggestion1.setAttribute()
+//     questionNumero7.innerHTML = "<label>" + element.question + "</label>";
+//     let getDiv7 = document.querySelector("body");
+//     getDiv7.appendChild(questionNumero7);
+// }
+
+for (element of questions) {
+    let question_div = document.createElement("div");
+    question_div.innerHTML = "<label>" + element.question + "</label><br/>";
+    let getDiv = document.querySelector("body");
+    getDiv.appendChild(question_div);
+
+
 }
+
 
 
 
