998212391d78555db3c978e21b54fbaa12c7c4c9
TanKARASU <karasutan@gmail.com>
2021-01-12 21:52:49 +0100

    projet ok

--- quizz/quizz.js
+++ quizz/quizz.js
@@ -21,8 +21,6 @@ let questions = [
   },
 ];
 
-
-
 for (const i of questions) {
   let { id, question, reponse } = i; // destructuring SUPER important
   generateDiv(id, "quizz");
@@ -37,10 +35,10 @@ for (const i of questions) {
   generateLabel("choix-" + id, id + "-1", c);
 }
 
-let bouttonValider = document.createElement("button")
+let bouttonValider = document.createElement("button");
 bouttonValider.setAttribute("type", "submit");
-bouttonValider.setAttribute("onclick", "showResult(event)")
-bouttonValider.textContent = "valider";
+bouttonValider.setAttribute("onclick", "showResult(event)");
+bouttonValider.textContent = "VERIFIER";
 let getForm = document.querySelector("form");
 getForm.appendChild(bouttonValider);
 
@@ -48,8 +46,8 @@ function generateRadio(name, idParent, value) {
   let bouttonRadio = document.createElement("input");
   bouttonRadio.setAttribute("type", "radio");
   bouttonRadio.setAttribute("name", name);
-  bouttonRadio.setAttribute("class", name)
-  bouttonRadio.setAttribute("value", value)
+  bouttonRadio.setAttribute("class", name);
+  bouttonRadio.setAttribute("value", value);
   let getDiv = document.getElementById(idParent);
   getDiv.appendChild(bouttonRadio);
 }
@@ -69,20 +67,41 @@ function generateDiv(idDivQuestion, idParent) {
   getDiv.appendChild(div);
 }
 
+// traitement du formulaire
 function showResult(event) {
   event.preventDefault();
-  const choixFiltre = document.querySelectorAll(".choix-q1");
+  let resultats = document.getElementById("resultats");
+  resultats.textContent = "";
+  // récupération des div contenant les questions
+  const questionsDiv = document.querySelectorAll("#quizz> div");
+  // initialisation du tableau qui recueillera les réponses
+  let goodAnswer = 0;
+  let index = 0;
+  // on va boucler sur les questions
+  for (const question of questionsDiv) {
+    // destructuring
+    let { id, correctReponse } = questions[index];
+    const choixFiltre = document.querySelectorAll(`.choix-${id}`);
+    //Parcours la liste des choix pour selectionner l'élément choisi
+    choixFiltre.forEach(element => {
+      if (element.checked) {
+        // on sélectionne le label
+        let questionLabel = element.parentNode.parentNode.querySelector(
+          "label"
+        );
+        let verifReponse = false;
+        // on push directement l'état des réponses
+        if (element.value === correctReponse) {
+          questionLabel.setAttribute("class", "bonneReponse");
+          goodAnswer++;
+        } else {
+          questionLabel.setAttribute("class", "mauvaiseReponse");
+        }
+      }
+    });
+    index++;
+  }
+  goodAnswer = `Vous avez ${goodAnswer} bonnes réponses`;
 
-  let userReponse = [];
-
-  //Parcours la liste des choix pour selectionner l'élément choisi
-  choixFiltre.forEach(element => {
-    if (element.checked) {
-      userReponse.push(element.value);
-    }
-  });
-
-console.log(userReponse);
-
-  
+  resultats.textContent = goodAnswer;
 }
