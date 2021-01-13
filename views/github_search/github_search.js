82dd9bdc6a740735ecbd3bc234fd05bb135b4a81
mchemlal <michel.chemlal@gmail.com>
2021-01-13 14:05:56 +0100

    insertion des infos sur profil via JS dans HTML

--- views/github_search/github_search.js
+++ views/github_search/github_search.js
@@ -9,10 +9,10 @@ fetch(github_URI).then(res => {
 
 
         let data = json;
-        console.log(data);
+
 
         let { avatar_url, followers, following, login, name, public_repos } = data;
-        console.log(name);
+
 
         // let avatar = document.createElement('img');
         // avatar.setAttribute("id", "avatar");
@@ -25,6 +25,17 @@ fetch(github_URI).then(res => {
         avatar.setAttribute("src", avatar_url);
         avatar.setAttribute("alt", "photo_profil_github");
 
+        let name_github = document.querySelector("#name");
+        name_github.textContent = name;
+
+        let following_github = document.querySelector("#following");
+        following_github.textContent = following;
+
+        let followers_github = document.querySelector("#followers");
+        followers_github.textContent = followers;
+
+        let public_repository = document.querySelector("#public_repository");
+        public_repository.textContent = public_repos;
 
 
 
