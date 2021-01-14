ef690824a575d6aa2077d507dc83981aa0cc6bf6
Samia Ouerfelli <73239375+souerfelli@users.noreply.github.com>
2021-01-14 10:17:10 +0100

    maj app météo

--- views/weather_app/weather.js
+++ views/weather_app/weather.js
@@ -23,19 +23,35 @@ fetch(IP_request)
         fetch(requestGeo)
             .then(res => res.json())
             .then(forecast => {
-                // destructure forecast pour extraire main
-                let { main } = forecast;
-                let temp = main.temp;
-                console.log(forecast);
-                // météo des jours suivants
+        // destructure forecast pour extraire main
+        let { main } = forecast;
+        let temp = main.temp;
+        console.log(forecast);
+        let villeActuelle = main.city;
 
-                // on code l'appli
+        let {description , icon} = forecast.weather[0]; 
 
-                let temperature = document.getElementById("temperature");
-                temperature.textContent = Math.ceil(temp * 2) / 2; // formule pour arrondir au demi superieur
+        let {speed} = forecast.wind;
+        
 
+        // météo des jours suivants
 
+        // on code l'appli
 
+        let temperature = document.getElementById("temperature");
+        temperature.textContent = Math.ceil(temp * 2) / 2; // formule pour arrondir au demi superieur
+
+        let ville = document.getElementById("ville");
+        ville.textContent = city;   
+
+        let descr = document.getElementById("description");
+        descr.textContent = description;
+
+        let iconWeather = document.getElementById("icon")
+        iconWeather.setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
+
+        let windSpeed = document.getElementById("vitesseVent");
+        windSpeed.textContent = Math.round(speed);
 
             });
     });
